e7a866e1a6defde13211e904ceefe27afebd1dde
Akon M Hasib <hasiburbd24@gmail.com>
2024-10-31 01:00:21 +0600

    new date

--- app/enroll/page.tsx
+++ app/enroll/page.tsx
@@ -42,7 +42,7 @@ const VALID_COUPON = "50OFF";
 const SUPER_COUPON = "SUPER80";
 
 const isSuperCouponValid = () => {
-  const expirationDate = new Date("2024-10-07");
+  const expirationDate = new Date("2024-11-07");
   return new Date() <= expirationDate;
 };
 
